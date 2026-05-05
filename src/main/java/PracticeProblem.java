5f5fb0ec3a96db0680eef89ee1553c233c2d7ede
usain-boltsMM <343421046@tdsb.ca>
2026-05-05 15:37:25 +0000

    KIA

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,28 +1,36 @@
-public static int findValue(int gm, int[] arr){
+public class PracticeProblem {
+
+	public static void main(String args[]) {
+
+	}
+
+    public static int findValue(int gm, int[] arr){
     for (int i = 0; i < arr.length; i++) {
         if (arr[i] == gm) {
             
             return i; } 
              }
-return -1;
+return -1;}
+    
  
- 
- public static int findThirdValue(String bhjm, String[] arr){
+ public static int findThirdValue(String bhjm, String[] arr) {
      int count = 0; 
      for (int i = 0; i <arr.length; i++){
          if (arr[i] == bhjm) {
              count++;
          }
-         if (count == 3) }{
+         if (count == 3) {
              return i;
-         }
+         
             
-         return -1
         }
-  
+     }
+  return -1;
+    
+    }
  
- public static double append(double gvh, double [] arr){
-    double[] array = new double[gvh.length+1]
+ public static double[] append(double gvh, double [] arr){
+    double[] array = new double[arr.length+1];
     
     for (int i = 0; i <arr.length; i++) {
         array[i] = arr[i];
@@ -30,13 +38,11 @@ return -1;
     }
     array[arr.length] = gvh;
      return array;
- }
+ 
     
  } 
-}public class PracticeProblem {
 
-	public static void main(String args[]) {
 
-	}
+
 
 }
