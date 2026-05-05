cf4cd3977efd581656a226e2e058274ce251a391
usain-boltsMM <343421046@tdsb.ca>
2026-05-05 11:25:12 -0400

    (P!@te PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,4 +1,39 @@
-public class PracticeProblem {
+public static int findValue(int gm, int[] arr){
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == gm) {
+            
+            return i; } 
+             }
+return -1;
+ 
+ 
+ public static int findThirdValue(String bhjm, String[] arr){
+     int count = 0; 
+     for (int i = 0; i <arr.length; i++){
+         if (arr[i] == bhjm) {
+             count++;
+         }
+         if (count == 3) }{
+             return i;
+         }
+            
+         return -1
+        }
+  
+ 
+ public static double append(double gvh, double [] arr){
+    double[] array = new double[gvh.length+1]
+    
+    for (int i = 0; i <arr.length; i++) {
+        array[i] = arr[i];
+        
+    }
+    array[arr.length] = gvh;
+     return array;
+ }
+    
+ } 
+}public class PracticeProblem {
 
 	public static void main(String args[]) {
 
